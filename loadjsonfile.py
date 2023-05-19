d9f1b428c85c1145cd3d43caa49b1fae4edcf73d
Nham Mai <104519047+nhammai102@users.noreply.github.com>
2023-05-19 17:33:17 +0700

    turn the json file to dictionary

---
+++ loadjsonfile.py
@@ -0,0 +1,26 @@
+import json
+
+def load_json_file(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+        
+    # Convert the 'amount' value to an integer
+    if "amount" in data:
+        data["amount"] = int(data["amount"])
+
+    return data
+
+# Use the function
+command = load_json_file('command.json')
+print(command)
+
+
+print(command["Command"])
+
+print(command["amount"])
+
+print(type(command["amount"]))
+
+
+
+
